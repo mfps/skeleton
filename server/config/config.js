06f3e1a10fb311c8e33ad517f041713c56bd2881
mfps <f.s.wille@gmail.com>
2016-02-02 14:11:44 +0100

    Update config.js

--- server/config/config.js
+++ server/config/config.js
@@ -1,6 +1,5 @@
 'use strict';
 module.exports = {
   TOKEN_SECRET: process.env.TOKEN_SECRET || 'A hard to guess string',
-  MONGO_URI: process.env.MONGO_URI || 'localhost:27017/mediadbv2',
-//  MONGO_URI: process.env.MONGO_URI || 'mongodb://mediadb:password666@ds063170.mongolab.com:63170/mediadb',
+  MONGO_URI: process.env.MONGO_URI || 'localhost:27017/test'
 };
